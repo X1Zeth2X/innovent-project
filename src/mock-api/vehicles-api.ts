72f0718349bc3a59b8e7ef8354f91d3bc4b07749
Zeth Leonardo <zeth.leonardo@protonmail.com>
2021-06-29 16:51:04 +0400

    Linting and added more mock-data.

--- src/mock-api/vehicles-api.ts
+++ src/mock-api/vehicles-api.ts
@@ -1,20 +1,32 @@
 // Import the Vehicles JSON file
+import { Car } from "@/store/types";
 import data from "./vehicles.json";
 
 // Create a new Vehicles Mock API
 class VehiclesAPI {
-  getVehicles = () => {
-    return new Promise((resolve, _reject) => {
+  getVehicles = (): Promise<Car[]> => {
+    // @TODO: Fake Pagination
+
+    return new Promise((resolve) => {
       // Simulate network (250ms) delay
       setTimeout(() => resolve(data.vehicles), 250);
     });
   };
 
   // Get a specific vehicle by its ID
-  getVehicle = (id: string) => {
-    return new Promise((_resolve, reject) => {
-      // Search the vehicles list
-      reject("IMPLEMENT");
+  getVehicle = (id: number): Promise<Car> => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        try {
+          // Filter vehicles and return with matching id.
+          const vehicle = data.vehicles.filter(
+            (vehicle) => vehicle.id == id
+          )[0];
+          resolve(vehicle);
+        } catch (error) {
+          reject(error);
+        }
+      }, 500);
     });
   };
 }
