780f73d7ae15f30311133fa2076848ddcc8f7d9a
Zeth Leonardo <zeth.leonardo@protonmail.com>
2021-06-29 13:55:27 +0400

    Create test getVehicles.

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -2,13 +2,17 @@
   <div id="landing">
     <Drawer />
 
-    <v-container> Landing page </v-container>
+    <v-container>
+      Landing page
+      <v-btn v-on:click="getVehicles"></v-btn>
+    </v-container>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
+import { Action } from "vuex-class";
 
 // Relative imports
 import Drawer from "@/components/Drawer.vue";
@@ -18,5 +22,9 @@ import Drawer from "@/components/Drawer.vue";
     Drawer,
   },
 })
-export default class Landing extends Vue {}
+export default class Landing extends Vue {
+  // vuex-class Action Method -> Calls list.getVehicles store method.
+  @Action("getVehicles", { namespace: "list" })
+  private getVehicles!: CallableFunction;
+}
 </script>
